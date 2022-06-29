a32adc41ac3974c75a26f91f1e5c084283f7bf5a
growbook91 <70497021+growbook91@users.noreply.github.com>
2022-06-29 17:06:15 +0900

    AppleSDGothic Font is added

--- lib/views/splash.dart
+++ lib/views/splash.dart
@@ -1,33 +1,3 @@
-// import 'package:animated_splash_screen/animated_splash_screen.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter/rendering.dart';
-// import 'package:flutter/src/foundation/key.dart';
-// import 'package:flutter/src/widgets/framework.dart';
-
-// class SplashScreen extends StatelessWidget {
-//   const SplashScreen({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: SafeArea(
-//         child: Center(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               Text("나만의 변비 관리 루틴 앱",style: TextStyle(fontFamily: 'GmarketSans', fontSize: 16),),
-//               SizedBox(height: 16,),
-//               Text("쾌변루틴",style: TextStyle(fontFamily: 'GmarketSans', fontSize: 36),),
-//               SizedBox(height: 250,),
-//               Text("LOADING...",style: TextStyle(fontFamily: 'GmarketSans', fontSize: 16),),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
