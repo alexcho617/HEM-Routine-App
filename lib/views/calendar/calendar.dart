1dc40bf6f7a4c2414d19d791822ad4202a6b89c4
Yechan Jung <asdxcv1998@gmail.com>
2022-07-14 17:29:19 +0900

    Update calendar.dart

--- lib/views/calendar/calendar.dart
+++ lib/views/calendar/calendar.dart
@@ -93,7 +93,7 @@ class _CalendarState extends State<Calendar> {
         SizedBox(
           height: 100.0.h,
         ),
-        PlusSquareButton(
+        plusSquareButton(
           () {
             Get.to(NewCalendarEvent());
           },
