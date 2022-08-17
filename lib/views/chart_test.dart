37fee4895da8342b9c4edec18245fb1e4aabb637
Yechan Jung <asdxcv1998@gmail.com>
2022-08-17 09:28:05 +0900

    Update chart_test.dart

--- lib/views/chart_test.dart
+++ lib/views/chart_test.dart
@@ -25,7 +25,7 @@ class _ChartTestPageState extends State<ChartTestPage> {
             Text("Chart Test Page"),
             Container(
               color: Colors.transparent,
-              child: circularChart(0.10, 0.67, 0.23),
+              child: circularAnalysisChart(0.10, 0.67, 0.23),
             ),
           ],
         ),
@@ -34,11 +34,11 @@ class _ChartTestPageState extends State<ChartTestPage> {
   }
 }
 
-Widget circularChart(double x1, double x2, double x3) {
-  List<ChartData> chartdatas = [
-    ChartData("묽은변", x1, color: blue50),
-    ChartData("쾌변", x2, color: blue600),
-    ChartData("단단한 변", x3, color: blue200),
+Widget circularAnalysisChart(double x1, double x2, double x3) {
+  List<AnalysisChartData> chartdatas = [
+    AnalysisChartData("묽은변", x1, color: blue50),
+    AnalysisChartData("쾌변", x2, color: blue600),
+    AnalysisChartData("단단한 변", x3, color: blue200),
   ];
   return SfCircularChart(
     annotations: <CircularChartAnnotation>[
@@ -83,9 +83,69 @@ Widget circularChart(double x1, double x2, double x3) {
   );
 }
 
-class ChartData {
-  ChartData(this.x, this.y, {this.color = Colors.pinkAccent});
+class AnalysisChartData {
+  AnalysisChartData(this.x, this.y, {this.color = Colors.pinkAccent});
   final String x;
+  final double y;
+  final Color color;
+}
+
+
+Widget circularColorChart(List<double> x) {
+  List<ColorChartData> chartdatas = [
+    ColorChartData(x[1],const Color.fromRGBO(0xD9, 0xD9, 0xD9, 1)),
+    ColorChartData(x[2],const Color.fromRGBO(0xDC, 0x6D, 0x69, 1)),
+    ColorChartData(x[3],const Color.fromRGBO(0x88, 0xA8, 0x83, 1)),
+    ColorChartData(x[4],const Color.fromRGBO(0xF6, 0xC1, 0x43, 1)),
+    ColorChartData(x[5],const Color.fromRGBO(0x7A, 0x46, 0x28, 1)),
+    ColorChartData(x[6],const Color.fromRGBO(0x63, 0x28, 0x2A, 1)),
+    ColorChartData(x[7],const Color.fromRGBO(0x59, 0x59, 0x59, 1)),
+  ];
+  return SfCircularChart(
+    // annotations: <CircularChartAnnotation>[
+    //   CircularChartAnnotation(
+    //     // angle: 0,
+    //     // radius: '0%',
+    //     widget: Column(
+    //       children: [
+    //         SizedBox(
+    //           height: 100.h,
+    //         ),
+    //         Text("이번 달 쾌변율", style: AppleFont14_Grey600,),
+    //         Text("${(x2 * 100).toStringAsFixed(0)}%",style: AppleFont36_Blue600,)
+    //       ],
+    //     ),
+    //   ),
+    // ],
+    legend: Legend(isVisible: true, position: LegendPosition.bottom),
+    series: <CircularSeries>[
+      DoughnutSeries(
+          dataSource: chartdatas,
+          startAngle: 270,
+          endAngle: 270,
+          pointColorMapper: (data, index) {
+            return data.color;
+          },
+          xValueMapper: (data, index) {
+            return "";
+          },
+          yValueMapper: (data, index) {
+            return data.y;
+          },
+          innerRadius: '75%',
+          dataLabelSettings: DataLabelSettings(
+            isVisible: true,
+            builder: (data, point, series, pointIndex, seriesIndex) {
+              return Text((data.y * 100).toStringAsFixed(0) + "%", style: TextStyle(color:white, fontSize: 14),);
+            },
+          ),
+          ),
+    ],
+  );
+}
+
+class ColorChartData{
+  ColorChartData(this.y, this.color);
   final double y;
   final Color color;
 }
